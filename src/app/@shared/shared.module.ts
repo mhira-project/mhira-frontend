ef04a9f4c6550ea0d423b12e153eab753988e729
Edgar Emmanuel <eedgar@gateway.co.tz>
2020-09-14 14:36:17 +0300

    added custom components

--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { XlsExportService } from '@shared/services/xls-export.service';
-import { LoaderComponent } from '@shared';
+import { LoaderComponent } from '@shared/components/loader/loader.component';
 import { CustomTableComponent } from '@shared/components/custom-table/custom-table.component';
 import { FormGeneratorComponent } from '@shared/components/form-generator/form-generator.component';
 
