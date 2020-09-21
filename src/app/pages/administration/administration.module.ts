5d2c2b32f4f581272c9302d6081e8fdea4c6f38e
Edgar Emmanuel <eedgar@gateway.co.tz>
2020-09-21 12:48:15 +0300

    fixed layout griches

--- src/app/pages/administration/administration.module.ts
+++ src/app/pages/administration/administration.module.ts
@@ -8,11 +8,19 @@ import { AdministrationComponent } from './administration.component';
 import { AdministrationRoutingModule } from './administration-routing.module';
 import { UserManagementComponent } from './user-management/user-management.component';
 import { RolesAndPermissionsComponent } from './roles-and-permissions/roles-and-permissions.component';
-import { NzModalModule, NzTabsModule } from 'ng-zorro-antd';
+import { NzGridModule, NzModalModule, NzTabsModule } from 'ng-zorro-antd';
 import { UserFormComponent } from './user-management/user-form/user-form.component';
 
 @NgModule({
-  imports: [CommonModule, TranslateModule, SharedModule, AdministrationRoutingModule, NzModalModule, NzTabsModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    SharedModule,
+    AdministrationRoutingModule,
+    NzModalModule,
+    NzTabsModule,
+    NzGridModule,
+  ],
   declarations: [AdministrationComponent, UserManagementComponent, RolesAndPermissionsComponent, UserFormComponent],
 })
 export class AdministrationModule {}
