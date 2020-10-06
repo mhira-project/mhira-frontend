678bde149964dbda8d4dd53c808f0539eff16753
Edgar Emmanuel <eedgar@gateway.co.tz>
2020-10-06 12:14:44 +0300

    questionnaire list view

--- src/app/questionnaire/questionnaire.module.ts
+++ src/app/questionnaire/questionnaire.module.ts
@@ -2,12 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { NzAlertModule, NzButtonModule, NzFormModule, NzInputModule, NzLayoutModule } from 'ng-zorro-antd';
+import {
+  NzAlertModule,
+  NzButtonModule,
+  NzFormModule,
+  NzGridModule,
+  NzInputModule,
+  NzLayoutModule,
+  NzCardModule,
+  NzIconModule,
+  NzToolTipModule,
+} from 'ng-zorro-antd';
 import { QuestionnairesListComponent } from './questionnaires-list/questionnaires-list.component';
 import { QuestionnaireRoutingModule } from './questionnaire-routing.module';
 import { QuestionComponent } from './question/question.component';
 
-const antModules = [NzFormModule, NzAlertModule, NzLayoutModule, NzInputModule, NzButtonModule];
+const antModules = [
+  NzFormModule,
+  NzAlertModule,
+  NzLayoutModule,
+  NzInputModule,
+  NzButtonModule,
+  NzGridModule,
+  NzCardModule,
+  NzIconModule,
+  NzToolTipModule,
+];
 
 @NgModule({
   imports: [...antModules, CommonModule, ReactiveFormsModule, TranslateModule, QuestionnaireRoutingModule],
--- src/app/questionnaire/questionnaires-list/questionnaires-list.component.ts
+++ src/app/questionnaire/questionnaires-list/questionnaires-list.component.ts
@@ -6,6 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./questionnaires-list.component.scss'],
 })
 export class QuestionnairesListComponent implements OnInit {
+  description =
+    "Hello Jon, thank your for answering the following assessment. The information you're providing is very important for a proper diagnosis and treatment. Please start the assessment by pressing the button below. The questionnaires are in fixed order";
+  questionnaires: any[] = [
+    {
+      name: 'Patient Health Questionnaire - 9',
+      summary: '9/9 Questions filled',
+      fillingTime: 2,
+      description: 'This is the simple questionnaire description',
+    },
+    {
+      name: 'PROMISE Pediatric Global Sclase',
+      summary: '14/20 Questions filled',
+      fillingTime: 2,
+      description: 'This is the simple questionnaire description',
+    },
+    {
+      name: 'Levels of Personality Function in Adolescents (LoPF-1218)',
+      summary: '0/4 Questions filled',
+      fillingTime: 2,
+      description: 'This is the simple questionnaire description',
+    },
+    {
+      name: 'Child behaviour Checklist',
+      summary: '0/15 Questions filled',
+      fillingTime: 2,
+      description: 'This is the simple questionnaire description',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
