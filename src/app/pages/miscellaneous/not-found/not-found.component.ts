fd8e93f4fc9d7bfeee15089fe688457f71875cd9
Edgar Emmanuel <eedgar@gateway.co.tz>
2020-10-19 15:56:28 +0300

    fixed fonts

--- src/app/pages/miscellaneous/not-found/not-found.component.ts
+++ src/app/pages/miscellaneous/not-found/not-found.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent implements OnInit {
+  user: any;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user')).user;
+  }
 }
