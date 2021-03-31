2eeadeedceac76d4c47be6c8b0a457bcd8dba047
Markus Chiarot <cmarkus@hotmail.ch>
2021-03-31 16:23:38 +0200

    fix(user): disable delete option if hierarchy is weaker than target

--- src/app/pages/user-management/users-list/users-list.component.ts
+++ src/app/pages/user-management/users-list/users-list.component.ts
@@ -121,6 +121,12 @@ export class UsersListComponent {
   }
 
   private async deleteUser(user: FormattedUser): Promise<void> {
+    // check if you're allowed to delete the user
+    if (!this.perms.hasAccessLevelToUser(user)) {
+      this.messageService.error("You don't have sufficient permission to delete this user", { nzDuration: 3000 });
+      return;
+    }
+
     const modal = this.modalService.confirm({
       nzOnOk: () => true,
       nzTitle: 'Delete User',
