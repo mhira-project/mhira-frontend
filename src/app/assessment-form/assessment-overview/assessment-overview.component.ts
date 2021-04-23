59ca81e6ef6c523786ab5f0d7c5bfd4d240f2f1f
Markus Chiarot <cmarkus@hotmail.ch>
2021-04-23 13:52:39 +0200

    feat(assessment): rework overview, add complete button and percentage

--- src/app/assessment-form/assessment-overview/assessment-overview.component.ts
+++ src/app/assessment-form/assessment-overview/assessment-overview.component.ts
@@ -1,7 +1,8 @@
 import { FullAssessment } from './../../pages/assessment/@types/assessment';
 import { Component } from '@angular/core';
-import { QuestionnaireVersion } from '../../pages/questionnaire-management/@types/questionnaire';
 import { AssessmentFormService } from '../assessment-form.service';
+import { Question } from '../@types/question';
+import { Answer } from '../@types/answer';
 
 @Component({
   selector: 'app-assessment-overview',
@@ -9,28 +10,60 @@ import { AssessmentFormService } from '../assessment-form.service';
   styleUrls: ['./assessment-overview.component.scss'],
 })
 export class AssessmentOverviewComponent {
-  constructor(public assessmentFormService: AssessmentFormService) {}
+  public assessment: FullAssessment;
+  public questions: Question[];
+  public questionnaireQuestions: { [K in string]: Question[] };
 
-  public countQuestions(questionnaire: QuestionnaireVersion) {
-    return questionnaire.questionGroups.reduce((sum, group) => (sum += group.questions.length), 0);
+  public get answers(): Answer[] {
+    return this.assessment.questionnaireAssessment.answers;
   }
 
-  public countAnswersOfQuestionnaire(questionnaire: QuestionnaireVersion, assessment: FullAssessment) {
-    const questions = questionnaire.questionGroups.map((qg) => qg.questions.map((q) => q._id)).flat();
-    return assessment.questionnaireAssessment.answers.reduce(
-      (sum: number, answer) => (sum += questions.includes(answer.question) ? 1 : 0),
+  public get requiredQuestions(): Question[] {
+    return this.questions.filter((q) => q.required);
+  }
+
+  public get percentageCompleted(): number {
+    const numAnswers = this.requiredQuestions.reduce(
+      (sum, question) => (this.answers.find((a) => a.question === question._id) ? (sum += 1) : sum),
+      0
+    );
+    return (numAnswers / this.requiredQuestions.length) * 100;
+  }
+
+  constructor(public assessmentFormService: AssessmentFormService) {
+    this.assessmentFormService.assessment$.subscribe((assessment) => {
+      this.assessment = assessment;
+      this.questionnaireQuestions = {};
+      this.questions = assessment.questionnaireAssessment.questionnaires.reduce((questions, questionnaire) => {
+        const questionnaireQuestions = questionnaire.questionGroups.reduce(
+          (qs, group) => [...qs, ...group.questions],
+          []
+        );
+        this.questionnaireQuestions[questionnaire._id] = questionnaireQuestions;
+        return [...questions, ...questionnaireQuestions];
+      }, []);
+    });
+  }
+
+  public getMaxQuestions(questionnaireId: string): number {
+    return this.questionnaireQuestions[questionnaireId].length;
+  }
+
+  public getAnsweredQuestions(questionnaireId: string): number {
+    return this.questionnaireQuestions[questionnaireId].reduce(
+      (sum, q) => (this.answers.find((a) => a.question === q._id) ? (sum += 1) : sum),
       0
     );
   }
 
-  public isQuestionnaireDone(questionnaire: QuestionnaireVersion, assessment: FullAssessment): boolean {
-    if (!questionnaire) return false;
-    return this.countQuestions(questionnaire) === this.countAnswersOfQuestionnaire(questionnaire, assessment);
+  public isQuestionnaireDone(questionnaireId: string): boolean {
+    const requiredQuestions = this.questionnaireQuestions[questionnaireId].filter((q) => q.required);
+    return requiredQuestions.every((q) => this.answers.find((a) => a.question === q._id));
   }
 
-  public canAccessQuestionnaire(index: number, assessment: FullAssessment): boolean {
-    if (index === 0) return true;
-    const questionnaire = assessment.questionnaireAssessment.questionnaires?.[index - 1];
-    return this.isQuestionnaireDone(questionnaire, assessment);
+  public canAccessQuestionnaire(questionnaireIdx: number): boolean {
+    if (questionnaireIdx === 0) return true;
+    const previousQuestionnaireId = this.assessment.questionnaireAssessment.questionnaires[questionnaireIdx - 1]._id;
+    return this.isQuestionnaireDone(previousQuestionnaireId);
   }
 }
