46f4837137fedbcca1358a2840029d2bb657b896
Edgar Emmanuel <eedgar@gateway.co.tz>
2021-05-03 08:58:31 +0300

    removed unable to delete new role bug

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ import { EnvironmentBase } from './environment.base';
 export const environment = {
   ...EnvironmentBase,
   hmr: true,
-  baseURL: 'https://dev-api.mhira.net/graphql', // 'http://localhost:3000/graphql',
+  baseURL: 'http://localhost:3000/graphql',
 };
