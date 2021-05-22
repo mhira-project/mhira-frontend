57042e38fc2ca356f2adc9e34bc720338c41a30a
Markus Chiarot <cmarkus@hotmail.ch>
2021-05-22 22:02:08 +0200

    feat(questionnaire): add archive/delete for questionnaire management

--- src/app/pages/questionnaire-management/questionnaire-list/questionnaire-list.component.ts
+++ src/app/pages/questionnaire-management/questionnaire-list/questionnaire-list.component.ts
@@ -1,15 +1,23 @@
 import { Convert } from '@shared/classes/convert';
 import { Component } from '@angular/core';
 import { QuestionnaireManagementService } from '../@services/questionnaire-management.service';
-import { FormattedQuestionnaireVersion } from '../@types/questionnaire';
-import { TableColumn } from '../../../@shared/@modules/master-data/@types/list';
+import { FormattedQuestionnaireVersion, QuestionnaireStatus } from '../@types/questionnaire';
+import { TableColumn, Action, ActionArgs } from '../../../@shared/@modules/master-data/@types/list';
 import { PermissionKey } from '../../../@shared/@types/permission';
 import { AppPermissionsService } from '../../../@shared/services/app-permissions.service';
 import { QuestionnaireColumns } from '../@tables/questionnaire.table';
 import { Router } from '@angular/router';
 import { environment } from '@env/environment';
+import { NzModalService } from 'ng-zorro-antd';
+import { finalize } from 'rxjs/operators';
+import { ErrorHandlerService } from '../../../@shared/services/error-handler.service';
 
 const CryptoJS = require('crypto-js');
+
+enum ActionKey {
+  ARCHIVE_QUESTIONNAIRE,
+  DELETE_QUESTIONNAIRE,
+}
 
 @Component({
   selector: 'app-questionnaire-list',
@@ -23,12 +31,25 @@ export class QuestionnaireListComponent {
 
   public columns: TableColumn<FormattedQuestionnaireVersion>[] = QuestionnaireColumns;
 
+  public actions: Action<ActionKey>[] = [];
+
+  public loading = false;
+
   constructor(
     private qmService: QuestionnaireManagementService,
     public perms: AppPermissionsService,
-    private router: Router
+    private router: Router,
+    private modalService: NzModalService,
+    private errorService: ErrorHandlerService
   ) {
     this.getQuestionnaires();
+
+    if (this.perms.permissionsOnly(PermissionKey.MANAGE_QUESTIONNAIRES)) {
+      this.actions.push({ key: ActionKey.ARCHIVE_QUESTIONNAIRE, title: 'Archive Questionnaire' });
+    }
+    if (this.perms.permissionsOnly(PermissionKey.DELETE_QUESTIONNAIRES)) {
+      this.actions.push({ key: ActionKey.DELETE_QUESTIONNAIRE, title: 'Delete Questionnaire' });
+    }
   }
 
   public onSelect(questionnaire: FormattedQuestionnaireVersion): void {
@@ -40,9 +61,61 @@ export class QuestionnaireListComponent {
     });
   }
 
+  public onAction({ action, context: questionnaire }: ActionArgs<FormattedQuestionnaireVersion, ActionKey>): void {
+    switch (action.key) {
+      case ActionKey.ARCHIVE_QUESTIONNAIRE:
+        this.deleteQuestionnaire(questionnaire);
+        return;
+      case ActionKey.DELETE_QUESTIONNAIRE:
+        this.deleteQuestionnaire(questionnaire, false);
+        return;
+    }
+  }
+
   private getQuestionnaires(): void {
-    this.qmService.getQuestionnaires({ status: null }).subscribe((questionnaires) => {
-      this.data = questionnaires.map((q) => Convert.toFormattedQuestionnaireVersion(q));
-    });
+    this.loading = true;
+    this.qmService
+      .getQuestionnaires({ status: null })
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe((questionnaires) => {
+        this.data = questionnaires.map((q) => Convert.toFormattedQuestionnaireVersion(q));
+      });
+  }
+
+  private async deleteQuestionnaire(
+    questionnaire: FormattedQuestionnaireVersion,
+    archive: boolean = true
+  ): Promise<void> {
+    if (!archive) {
+      // create confirmation modal
+      const modal = this.modalService.confirm({
+        nzOnOk: () => true,
+        nzTitle: 'Delete Questionnaire',
+        nzContent: `
+        Are you sure you want to delete ${questionnaire.name}? This action is irreversible
+      `,
+      });
+
+      // wait for modal to successfully complete
+      const confirmation = await modal.afterClose.toPromise();
+      if (!confirmation) return;
+    }
+
+    this.loading = true;
+    this.qmService
+      .deleteQuestionnaire(questionnaire.questionnaire._id, archive)
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe(
+        () => {
+          if (archive) {
+            questionnaire.status = QuestionnaireStatus.ARCHIVED;
+            questionnaire = Convert.toFormattedQuestionnaireVersion(questionnaire);
+          } else {
+            this.data.splice(this.data.indexOf(questionnaire), 1);
+          }
+        },
+        (error) =>
+          this.errorService.handleError(error, { prefix: `Unable to delete assessment "${questionnaire.name}"` })
+      );
   }
 }
