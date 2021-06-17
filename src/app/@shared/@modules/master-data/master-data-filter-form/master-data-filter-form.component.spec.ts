a0f2ed81313c1ca58db4f40066f40e7f1b8f78b7
Markus Chiarot <cmarkus@hotmail.ch>
2021-06-17 15:24:32 +0200

    fix: restore deleted spec files

---
+++ src/app/@shared/@modules/master-data/master-data-filter-form/master-data-filter-form.component.spec.ts
@@ -0,0 +1,26 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MasterDataFilterFormComponent } from './master-data-filter-form.component';
+
+describe('MasterDataFilterFormComponent', () => {
+  let component: MasterDataFilterFormComponent<any>;
+  let fixture: ComponentFixture<MasterDataFilterFormComponent<any>>;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [MasterDataFilterFormComponent],
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MasterDataFilterFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
