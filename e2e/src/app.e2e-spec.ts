ecee5edf826790f523246a61d222a4aaa93500c8
erald <eraldlifo992gmail.com>
2021-12-03 16:56:31 +0100

    add testing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,11 +2,15 @@ import { browser, ExpectedConditions as until } from 'protractor';
 import { LoginPage } from './page-objects/login.po';
 import { AppSharedPage } from './page-objects/app-shared.po';
 import { ShellPage } from './page-objects/shell.po';
+import { DepartmentsPo } from './page-objects/departments.po';
+import { UsersPo } from './page-objects/users.po';
 
 describe('when the app loads', () => {
   const login = new LoginPage();
   const app = new AppSharedPage();
   const shell = new ShellPage();
+  const department = new DepartmentsPo();
+  const user = new UsersPo();
 
   beforeAll(async () => {
     await app.navigateAndSetLanguage();
@@ -23,7 +27,16 @@ describe('when the app loads', () => {
 
     it('should display the hello message', async () => {
       await browser.wait(until.visibilityOf(shell.welcomeText), 5000, 'Element taking too long to appear');
-      expect(await shell.getParagraphText()).toEqual('Hello world !');
+      expect(await shell.getParagraphText()).toEqual('Welcome to MHIRA');
     });
+  });
+  it('should send to departments', async () => {
+    await browser.get('/mhira/administration/departments');
+    await browser.wait(until.visibilityOf(shell.departmentText), 5000);
+    await department.createDepartment('E2ETest2');
+  });
+  it('should create a user with the  permissions to create and view patients', async () => {
+    await browser.get('/mhira/user-management/user-form');
+    await user.createUser();
   });
 });
