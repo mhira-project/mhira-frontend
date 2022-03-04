282d31f3b9a1d78d08cb1080fb7e5750b5c58117
Erald-Whitey <eraldlifo99@gmail.com>
2022-03-04 18:17:08 +0100

    last changes

--- src/app/pages/questionnaire-management/questionnaire-management.module.ts
+++ src/app/pages/questionnaire-management/questionnaire-management.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { QuestionnaireFormComponent } from './questionnaire-form/questionnaire-form.component';
+import { QuestionnaireFormComponent } from './questionnaire-profile/questionnaire-form/questionnaire-form.component';
 import { QuestionnaireManagementRoutingModule } from './questionnaire-management-routing.module';
 import { FormsModule } from '@angular/forms';
 import { QuestionnaireListComponent } from './questionnaire-list/questionnaire-list.component';
@@ -10,9 +10,16 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { QuestionnaireProfileComponent } from './questionnaire-profile/questionnaire-profile.component';
+import { QuestionnaireScriptComponent } from './questionnaire-script/questionnaire-script.component';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 
 @NgModule({
-  declarations: [QuestionnaireFormComponent, QuestionnaireListComponent, QuestionnaireProfileComponent],
+  declarations: [
+    QuestionnaireFormComponent,
+    QuestionnaireListComponent,
+    QuestionnaireProfileComponent,
+    QuestionnaireScriptComponent,
+  ],
   imports: [
     CommonModule,
     QuestionnaireManagementRoutingModule,
@@ -22,6 +29,7 @@ import { QuestionnaireProfileComponent } from './questionnaire-profile/questionn
     NzIconModule,
     AppFormModule,
     NzTabsModule,
+    NzDrawerModule,
   ],
 })
 export class QuestionnaireManagementModule {}
