9398ad02563c8ff2e08cdd1556dd57749bbd4406
Erald-Whitey <eraldlifo99@gmail.com>
2022-04-14 14:22:54 +0200

    Testing

--- src/app/pages/questionnaire-management/questionnaire-version-list/questionnaire-version-list.component.spec.ts
+++ src/app/pages/questionnaire-management/questionnaire-version-list/questionnaire-version-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { QuestionnaireVersionListComponent } from './questionnaire-version-list.component';
 
@@ -6,11 +6,13 @@ describe('QuestionnaireVersionListComponent', () => {
   let component: QuestionnaireVersionListComponent;
   let fixture: ComponentFixture<QuestionnaireVersionListComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [QuestionnaireVersionListComponent],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [QuestionnaireVersionListComponent],
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(QuestionnaireVersionListComponent);
