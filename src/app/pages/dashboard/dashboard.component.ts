b64b8fc4e856c05fac0e346ec4f5784a2e169dda
Erald-Whitey <eraldlifo99@gmail.com>
2022-04-26 19:04:44 +0200

    Login disclaimer

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MhiraTranslations } from '../../@core/mhira-translations';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { Disclaimers } from '@app/pages/administration/@types/disclaimers';
+import { DisclaimersService } from '@app/pages/administration/@services/disclaimers.service';
+import { finalize } from 'rxjs/operators';
+import { ErrorHandlerService } from '@shared/services/error-handler.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
-  constructor(public translations: MhiraTranslations) {}
+export class DashboardComponent implements OnInit {
+  isVisible = true;
+  public disclaimer: Disclaimers;
+  public data: Partial<Disclaimers>[];
+  public isLoading = false;
+
+  constructor(
+    public translations: MhiraTranslations,
+    private disclaimersService: DisclaimersService,
+    private errorService: ErrorHandlerService,
+    private modal: NzModalService
+  ) {}
+
+  ngOnInit(): void {
+    this.getDescription();
+    console.log(this.disclaimer);
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  private getDescription(): void {
+    this.disclaimersService
+      .disclaimers()
+      .pipe(finalize(() => (this.isLoading = false)))
+      .subscribe(
+        ({ data }: any) => {
+          console.log(data);
+          this.disclaimer = data.disclaimers.find((disclaimers: any) => disclaimers.type === 'loginDisclaimer');
+        },
+        (err) => this.errorService.handleError(err, { prefix: 'Unable to load disclaimers' })
+      );
+  }
 }
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -5,9 +5,10 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { TranslateModule } from '@ngx-translate/core';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { ReportsComponent } from './reports/reports.component';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 @NgModule({
   declarations: [DashboardComponent, ReportsComponent],
-  imports: [CommonModule, DashboardRoutingModule, TranslateModule.forChild(), NzTabsModule],
+  imports: [CommonModule, DashboardRoutingModule, TranslateModule.forChild(), NzTabsModule, NzModalModule],
 })
 export class DashboardModule {}
