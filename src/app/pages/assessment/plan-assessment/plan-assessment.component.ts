b55e7764724e0a8a5164c80a5a3b3366e28b0ee3
Alteo <alteo.caka@atis.al>
2022-12-23 12:47:37 +0100

    Removed email placeholder

--- src/app/pages/assessment/plan-assessment/plan-assessment.component.ts
+++ src/app/pages/assessment/plan-assessment/plan-assessment.component.ts
@@ -432,6 +432,7 @@ export class PlanAssessmentComponent implements OnInit {
             },
           ];
         }
+        console.log(this.fullAssessment)
       },
       (error) =>
         this.errorService.handleError(error, { prefix: `Unable to load the assessment with ID "${assessmentId}"` })
