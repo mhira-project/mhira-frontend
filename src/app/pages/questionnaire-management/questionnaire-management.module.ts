8183f21331af34370cbe55ec1fc79a0c76369412
Alteo <alteo.caka@atis.al>
2023-03-17 15:43:35 +0100

    Fixed missing module

--- src/app/pages/questionnaire-management/questionnaire-management.module.ts
+++ src/app/pages/questionnaire-management/questionnaire-management.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuestionnaireFormComponent } from './questionnaire-profile/questionnaire-form/questionnaire-form.component';
 import { QuestionnaireManagementRoutingModule } from './questionnaire-management-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { QuestionnaireListComponent } from './questionnaire-list/questionnaire-list.component';
 import { MasterDataModule } from '../../@shared/@modules/master-data/master-data.module';
 import { AppFormModule } from '../../@shared/components/form/app-form.module';
@@ -13,33 +13,48 @@ import { QuestionnaireProfileComponent } from './questionnaire-profile/questionn
 import { QuestionnaireScriptComponent } from './questionnaire-script/questionnaire-script.component';
 import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { QuestionnaireVersionListComponent } from './questionnaire-version-list/questionnaire-version-list.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { CreateQuestionnaireBundleComponent } from './create-questionnaire-bundle/create-questionnaire-bundle.component';
+import { QuestionnaireBundlesListComponent } from './questionnaire-bundles-list/questionnaire-bundles-list.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { AssessmentModule } from "../assessment/assessment.module";
 
 @NgModule({
-  declarations: [
-    QuestionnaireFormComponent,
-    QuestionnaireListComponent,
-    QuestionnaireProfileComponent,
-    QuestionnaireScriptComponent,
-    QuestionnaireVersionListComponent,
-  ],
-  imports: [
-    CommonModule,
-    QuestionnaireManagementRoutingModule,
-    FormsModule,
-    MasterDataModule,
-    NzButtonModule,
-    NzIconModule,
-    NzSelectModule,
-    AppFormModule,
-    NzTabsModule,
-    NzDrawerModule,
-    NzCardModule,
-    NzCheckboxModule,
-    TranslateModule,
-  ],
+    declarations: [
+        QuestionnaireFormComponent,
+        QuestionnaireListComponent,
+        QuestionnaireProfileComponent,
+        QuestionnaireScriptComponent,
+        QuestionnaireVersionListComponent,
+        CreateQuestionnaireBundleComponent,
+        QuestionnaireBundlesListComponent,
+    ],
+    imports: [
+        CommonModule,
+        QuestionnaireManagementRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MasterDataModule,
+        NzButtonModule,
+        NzGridModule,
+        NzIconModule,
+        NzSelectModule,
+        AppFormModule,
+        NzTabsModule,
+        NzDrawerModule,
+        NzCardModule,
+        NzCheckboxModule,
+        NzToolTipModule,
+        NzFormModule,
+        NzInputModule,
+        TranslateModule,
+        AssessmentModule
+    ]
 })
 export class QuestionnaireManagementModule {}
