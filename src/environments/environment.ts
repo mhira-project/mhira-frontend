05d872735751efdc2983ceca4f1db8588f5eca5b
Alteo Caka <alteo.caka@gmail.com>
2023-05-05 09:43:25 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,5 @@ export const environment = {
   ...EnvironmentBase,
   hmr: true,
   email: true,
-  // baseURL: 'http://localhost:3000/graphql',
-  baseURL: 'http://mhira.leetodev.me/graphql',
+  baseURL: 'http://localhost:3000/graphql',
 };
