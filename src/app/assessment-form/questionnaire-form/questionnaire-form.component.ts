167bf58d1526dc94f54d8ae1deda873b2c40fce8
Alteo Caka <alteo.caka@gmail.com>
2023-07-07 09:38:26 +0200

    Table list fixes for multiple groups, scroll issue fixed

--- src/app/assessment-form/questionnaire-form/questionnaire-form.component.ts
+++ src/app/assessment-form/questionnaire-form/questionnaire-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { QuestionnaireVersion } from '../../pages/questionnaire-management/@types/questionnaire';
 import { AssessmentFormService } from '../assessment-form.service';
 import { ActivatedRoute } from '@angular/router';
@@ -21,7 +21,7 @@ export class QuestionnaireFormComponent {
   public questionnaire: any;
   public answers: Answer[];
   public skipLogic: Array<{ questionId: string; visible: boolean }> = [];
-  selectedValues: any[] = [];
+  public mapped: any = {};
 
   public set currentGroupIdx(idx: number) {
     this._currentGroupIdx = idx;
@@ -53,7 +53,7 @@ export class QuestionnaireFormComponent {
           const questions: any[] = []
           const uniqueQuestions = {};
           group.questions.map((question: { appearance: string; choices: any[]; }) => {
-              if (question.appearance?.toLowerCase() == 'table-list') {
+              if (group.appearance?.toLowerCase() == 'table-list') {
                   const choices = question.choices.map((choice: { label: any; }) => choice.label)
                   if (!uniqueQuestions[JSON.stringify(choices)]) {
                       uniqueQuestions[JSON.stringify(choices)] = {
@@ -82,10 +82,18 @@ export class QuestionnaireFormComponent {
       })
         this.questionnaire = assessment?.questionnaireAssessment?.questionnaires?.[idx];
         this.answers = assessment?.questionnaireAssessment?.answers;
-        this.selectedValues = assessment?.questionnaireAssessment?.answers.map((el: any) => el.textValue);
+        this.answers.forEach(item => {
+          const questionKey = item.question;
+          this.mapped[questionKey] = item.textValue;
+        });
         this.assessmentFormService.setQuestionnaire(this.questionnaire);
         this.readSkipLogic();
       });
+  }
+
+  @HostListener('click')
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 
   public isVisible(question: Question) {
@@ -94,7 +102,6 @@ export class QuestionnaireFormComponent {
   }
 
   addAnswer(questionId: string, value: string){
-    console.log(questionId, value);
     value = value.toString();
     this.assessmentFormService.addAnswer({question: questionId, textValue: value}).subscribe()
   }
