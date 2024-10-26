c36a6993b4359cc80ea0ef8867c91f5ed00d63d8
edisoni1995 <edisonbiba91@gmail.com>
2024-10-26 18:18:19 +0200

    add tenant id in requests

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,12 +3,26 @@ import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { environment } from '../environments/environment';
+import { setContext } from 'apollo-link-context';
 
+const hostnames = window.location.hostname.split('.');
+let tenantID = '';
+if (hostnames.length > 2) {
+  tenantID = hostnames[0];
+}
 const uri = environment.baseURL;
 
 export function createApollo(httpLink: HttpLink) {
+  const auth = setContext((operation, context) => ({
+    headers: {
+      'x-tenant-id': tenantID,
+    },
+  }));
+
+  const http = httpLink.create({ uri });
+
   return {
-    link: httpLink.create({ uri }),
+    link: auth.concat(http),
     cache: new InMemoryCache(),
   };
 }
